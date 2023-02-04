8508139346e7360cbe5baaaafc79ae1a9f277514
markyevansy <fedihaha@gmail.com>
2023-02-05 00:50:52 +0100

    waterfall

---
+++ Scenes/waterfall.gd
@@ -0,0 +1,15 @@
+extends Polygon2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	texture_offset.y-=50*delta
